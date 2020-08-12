72769e1029b0e3fe2b4ae73a60a401f2c61722ee
Ross McKerchar <ross.mckerchar@sophos.com>
2020-08-12 18:39:15 +0100

    Fix merge

--- lights_away.py
+++ lights_away.py
@@ -2,30 +2,6 @@
 import random
 from shelly import *
 
-logging.basicConfig(level=logging.INFO)
-
-shellys = find_shellys(config.subnet)
-logging.info('Found %s shellies' % len(shellys))
-for shelly in shellys:
-    try:
-        name, relays = lookup_shelly_config(shelly['mac'])
-    except TypeError:
-        logging.info('Found shelly not in config: %s'%shelly)
-    else:
-        ip = shelly['wifi_sta']['ip']
-        logging.debug('Found known shelly %s, %s, %s, including relays %s'%(name, ip, shelly["mac"], relays))
-        for relay in relays:
-            if random.random() < config.prob:
-                on_for = random.randint(config.min_on,config.max_on)
-                logging.info('Turning on relay %s for shelly %s for %s seconds'%(relay, name, on_for))
-                #set_timer(ip, relay, on_for)
-            else:
-                logging.info('NOT turning on relay %s for shelly %s'%(relay, name))
-=======
-#!/usr/bin/env python3
-import random
-from shelly import *
-
 logging.basicConfig(level=config.log_level)
 shellys = find_shellys(config.subnet)
 
@@ -44,5 +20,3 @@ for shelly in shellys:
                 set_timer(ip, relay, on_for)
             else:
                 logging.info('NOT turning on relay %s for shelly %s'%(relay, name))
-
-#test change 2
